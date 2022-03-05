1d1b3db34efdc51728ffef0389f08a89a29699bd
João Victor Pereira da Silva <vics@ynko.onmicrosoft.com>
2022-03-05 20:40:02 -0300

    Add project files.

---
+++ OddTextGenerator/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OddTextGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = bs();
+        }
+
+        private string bs()
+        {
+           return GetRandomString();
+        }
+
+        public static String GetRandomString()
+        {
+            var allowedChars = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789";
+            var length = 15;
+
+            var chars = new char[length];
+            var rd = new Random();
+
+            for (var i = 0; i < length; i++)
+            {
+                chars[i] = allowedChars[rd.Next(0, allowedChars.Length)];
+            }
+
+            return new String(chars);
+        }
+    }
+}
